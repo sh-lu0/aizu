70969acf328a68bd5d8ada8d655cf9266fc183fe
sh-lu0 <sh-lu0@users.noreply.github.com>
2018-12-16 12:57:49 +0900

    高速化・通るように修正

--- ALDS1_1_C.php
+++ ALDS1_1_C.php
@@ -1,5 +1,3 @@
-/* Time Limit Exceeded!!! :_( */
-
 <?php
 $stdins = array();
 while ( !feof(STDIN) ) {
@@ -10,32 +8,37 @@ while ( !feof(STDIN) ) {
     $stdins[] = $stdin;
 }
 
-
 $lengthSequence = $stdins[0];
 $primeCount = 0;
 
 for ($i=1; $i<=$lengthSequence; $i++) {
-    if(CheckPrimeNumber($stdins[$i])==true){
+    if(CheckPrimeNumber($stdins[$i])==1){
         $primeCount++;
     }
 }
-print($primeCount);
-
+echo $primeCount."\n";
 
 function CheckPrimeNumber($number){
-    if($number<2) {
-        return false;
+    // nが1以下ならfalse
+    if($number<=1){
+        return 1;
     }
-    if($number==2) {
-        return true;
+    // nが2または3ならtrue
+    if(2==$number || 3==$number){
+        return 1;
     }
-
-    for( $j=2; $j<$number; $j++ ) {
-        if( $number%$j == 0 ) {
-            return false;
+    // nが2の倍数ならfalse
+    if(0==$number%2){
+        return 0;
+    }
+    // nの平方根を整数で取得(5なら2, 10なら3)
+    $square_root = (int)sqrt($number);
+    // nがnの平方根以下の、3以上の奇数(3,5,7,9,11,...)で1回でも割れたらfalse
+    for($k=3; $k<=$square_root; $k+=2){
+        if(0==$number%$k){
+            return 0;
         }
     }
-    return true;
+    // 上記すべて回避したらtrue
+    return 1;
 }
-
-?>
