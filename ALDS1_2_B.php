a506dd18300357f2f335a8ccbcab7978d7d8107b
sh-lu0 <sh-lu0@users.noreply.github.com>
2018-12-17 10:57:03 +0900

    ALDS1_2_B

---
+++ ALDS1_2_B.php
@@ -0,0 +1,27 @@
+<?php
+$Arraylength = trim(fgets(STDIN));
+$stdins = explode(" ",trim(fgets(STDIN)));
+
+selectionSort($stdins,$Arraylength);
+
+function selectionSort($Array, $N){
+    $count = 0;
+    for ($i=0; $i<$N; ++$i){
+        $mini = $i;
+        for ($j=$i; $j<$N; ++$j){
+            if ($Array[$j] < $Array[$mini]){
+                $mini = $j;
+            }
+        }
+        if($mini!=$i){
+            $tmp = $Array[$i];
+            $Array[$i] = $Array[$mini];
+            $Array[$mini] = $tmp;
+            $count++;
+        }
+    }
+    echo implode(" ",$Array) . PHP_EOL;
+    echo $count . PHP_EOL;
+}
+
+?>
