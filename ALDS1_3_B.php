73b5ff4e5f855007af925766e7c48d3fa5d4fc78
sh-lu0 <sh-lu0@users.noreply.github.com>
2018-12-18 23:18:59 +0900

    Time Limit Exceeded!!:_(

--- ALDS1_3_B.php
+++ ALDS1_3_B.php
@@ -1,34 +1,38 @@
 <?php
 $stdin = explode(" ",fgets(STDIN));
-$length = $stdin[0];
+$stdinLength = (int) $stdin[0];
 $quantum = (int) $stdin[1];
 $time = 0;
 
-for($i=0;$i<$length;++$i){
+for($i=0;$i<$stdinLength;++$i){
     $input = explode(" ",fgets(STDIN));
     $times[] = (int) $input[1];
-    $task[] = $input[0];
+    $taskNames[] = $input[0];
 }
 
-$result = "";
-
-while(count($times)!=0){
-    if(($times[0] - $quantum)<=0){
+while(count($times)!==0){
+    $result = (int) $times[0] - $quantum;
+    if($result<=0){
+        //プロセス完了
         $time += $times[0];
 
-        $result = $result . $task[0] . " ". $time . PHP_EOL;
+        echo $taskNames[0] . ' '. $time . PHP_EOL;
 
         array_splice($times, 0, 1);
-        array_splice($task, 0, 1);
-    } else{
-        $times[] = ($times[0] - $quantum);
-        $task[] = $task[0];
+        array_splice($taskNames, 0, 1);
+    } else {
+        //プロセス未完了
         $time += $quantum;
 
+        $times[] = $result;
+        $taskNames[] = $taskNames[0];
         array_splice($times, 0, 1);
-        array_splice($task, 0, 1);
+        array_splice($taskNames, 0, 1);
     }
 }
+
+?>
+
 
 
 echo $result;
