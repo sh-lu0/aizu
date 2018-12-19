4e5f282f77120747f93e1fbbbae512bd2e8938a6
sh-lu0 <sh-lu0@users.noreply.github.com>
2018-12-19 23:59:46 +0900

    途中

---
+++ ALDS1_4_A.php
@@ -0,0 +1,22 @@
+<?php
+$lengthA = fgets(STDIN);
+print($lengthA);
+$stdinA = explode(" ",fgets(STDIN));
+print_r($stdinA);
+$lengthKey = fgets(STDIN);
+print($lengthKey);
+$stdinKey = explode(" ",fgets(STDIN));
+print_r($stdinKey);
+
+
+
+function linearSearch($stdinA, key){
+    $i = 0;
+    $A[n] = key
+    while A[i] と key が異なる
+      i++
+    if i が n に達した
+      return NOT_FOUND
+    return i
+}
+?>
