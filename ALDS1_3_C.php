b166e0034266fedf783798e936130c389d3f08ad
sh-lu0 <sh-lu0@users.noreply.github.com>
2019-01-03 23:08:20 +0900

    ALDS1_3_C・3問目でエラー

---
+++ ALDS1_3_C.php
@@ -0,0 +1,22 @@
+<?php
+fscanf(STDIN, '%d', $length);
+$list = array();
+
+for($i=0; $i<$length; ++$i){
+    fscanf(STDIN, '%s %s', $action, $str);
+    switch ($action) {
+    case 'insert':
+        array_unshift($list,$str);
+        break;
+    case 'delete':
+        $key=array_search($str, $list);
+        unset($list[$key]);
+        break;
+    case 'deleteFirst':
+        array_shift($list);
+        break;
+    case 'deleteLast':
+        array_pop($list);
+        break;
+    }
+}
